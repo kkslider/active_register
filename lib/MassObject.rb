1fe58ad32379a54131f9d5591f1131665f681156
Benjamin Lee <benjamsandwich@gmail.com>
2013-10-07 11:59:03 -0700

    Finish Phase I

--- lib/MassObject.rb
+++ lib/MassObject.rb
@@ -7,8 +7,20 @@ class MassObject
     @attributes ||= []
   end
 
+  def initialize(params)
+    params.each do |attr_name, value|
+      if self.class.attributes.include?(attr_name)
+        self.send("#{attr_name}=", value)
+      else
+        raise "mass assignment to unregistered attribute #{attr_name}"
+      end
+    end
+  end
+
+
   def self.my_attr_accessible(*attributes)
     attributes.each do |attribute|
+      self.attributes << attribute
       attr_accessor(attribute)
     end
   end
@@ -18,9 +30,9 @@ class MyClass < MassObject
   my_attr_accessible :x, :y
 end
 
-my_obj = MyClass.new
-my_obj.x = :x_val
-my_obj.y = :y_val
-
+my_obj = MyClass.new(:x => :x_val, :y => :y_val)
+#my_obj = MyClass.new
+#my_obj.x = :x_val
+#my_obj.y = :y_val
 p my_obj.x
 p my_obj.y
