334836ea39135a714e1126f25b363f804dcd8ede
Benjamin Lee <benjamsandwich@gmail.com>
2013-10-07 14:00:43 -0700

    Remove files, implement code in active_record_lite folder

--- lib/active_record_lite/mass_object.rb
+++ lib/active_record_lite/mass_object.rb
@@ -1,13 +1,25 @@
 class MassObject
   def self.my_attr_accessible(*attributes)
+    attributes.each do |attribute|
+      self.attributes << attribute
+      attr_accessor(attribute)
+    end
   end
 
   def self.attributes
+    @attributes ||= []
   end
 
   def self.parse_all(results)
   end
 
   def initialize(params = {})
+    params.each do |attr_name, value|
+      if self.class.attributes.include?(attr_name.to_sym)
+        self.send("#{attr_name}=", value)
+      else
+        raise "mass assignment to unregistered attribute #{attr_name}"
+      end
+    end
   end
 end
--- lib/active_record_lite/sql_object.rb
+++ lib/active_record_lite/sql_object.rb
@@ -5,12 +5,19 @@ require_relative './searchable'
 
 class SQLObject < MassObject
   def self.set_table_name(table_name)
+    @current_table.pluralize.underscore
   end
 
   def self.table_name
+    @current_table.pluralize.underscore
   end
 
   def self.all
+    DBConnection.open(self.table_name)
+    results = DBConnection.execute("select * from ?", "#{self.table_name}")
+    results.each do |row|
+      self.new(row)
+    end
   end
 
   def self.find(id)
